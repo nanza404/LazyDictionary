d9c1c898ff53cecdda7a6dd59a815d9c86da1253
olegK <oakibalko@gmail.com>
2021-09-22 13:32:35 -0700

    fixed a few bugs from refactoring deleted weird cache file

--- htmlparse.py
+++ htmlparse.py
@@ -1,54 +1,28 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import re
 
-
-
-
-
-
-
-
-
-
-
+url = 'https://www.merriam-webster.com/dictionary/' 
+word = ''
 
 def wordLogger():
-    source = requests.get(url).text
-    soup = bs(source, 'lxml')
-    definition = ''
-    definit = ''
     curr_entry = 'dictionary-entry-1'
+    # grabs the html of the url
+    source = requests.get(url).text
+    # parses the html
+    soup = bs(source, 'lxml')
+    # edit up to the first dictionary entery and make it more managable
+    # (maybe dont strip strings)
     string_section = soup.find('div', id=curr_entry).stripped_strings
-    mainWord = True 
-    colon = False
+    return string_section
 
-    for string in string_section:
-        if re.search('Entry \d of \d', string):
-            def_num = re.findall('\d', string)
-            while int(def_num[0]) <= int(def_num[1]):
-                string_section = soup.find('div', id=curr_entry).stripped_strings
-                for string in string_section:
-                    if string == ('Definition of'):
-                        pass
-                    elif (string == word and mainWord): 
-                        definit += ('---'+string+'---\n')
-                        mainWord = False
-                    elif re.search('^..+.$', string):
-                        definit += string+' '
-                    elif ord(string) in range(49, 58) or ord(string) in range(97, 110):
-                        definit += '\n'+string
-                        colon = True
-                    elif (string == ':' and colon):
-                        definit += string+' '
-                        colon = False
-                    else:
-                        definit += string
-                #the list definit needs to be earsed after it is printed
-                print(definit)
-                definition += definit
-                definit = ''
-                def_num[0] = str(int(def_num[0])+1)
-                curr_entry = 'dictionary-entry-'+def_num[0]
 
-#opens up a file to append the dictionary definitions
+def parse():
+    return 0
+
+
+def writeToDictFile():
+    # opens up a file to append the dictionary definitions
     with open('Dictionary.txt', 'a') as f:
         f.write(definition)
 
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from keylog import OnKeyPress as kp
-#import keylog.py as kp
+import htmlparse as hp 
 import pyperclip as pc
 import webbrowser as wb
 
 class Dict:
-    global url
-    global word    
+    url = 'https://www.merriam-webster.com/dictionary/' 
+    word = ''
     
     def paste():
-        #paste should be an input from a different file
         word = pc.paste()
+        return word
     
     
     def openDict():
         if kp == True:
-            url = 'https://www.merriam-webster.com/dictionary/' + word 
+            url = 'https://www.merriam-webster.com/dictionary/' + paste() 
+            # opens up the url in a web browser
             wb.open_new(url)
-            wordLogger()
-    
-    
-    
+            # calls the html parse function
+            hp.wordLogger()
+
+
