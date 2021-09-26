0b54d530504de18c17ae4da58c94ffcb880bcd1f
nanza404 <oakibalko@gmail.com>
2021-09-26 00:19:07 -0700

    cleaned up the repo a bit deleted old test files

--- file.py
+++
@@ -1,5 +0,0 @@
-#opens up a file to append the dictionary definitions
-with open('Dictionary.txt', 'a') as f:
-    #have the write logic here(maybe have it all tucked away in a function)
-    f.write('hello world')
-
