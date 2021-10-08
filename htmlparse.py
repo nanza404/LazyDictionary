ad6076707380e0939790c6f6ce66421102e58fd9
nanza404 <oakibalko@gmail.com>
2021-10-07 23:44:34 -0700

    parse works and passes definitions to another function that writes definitions to dictionary file with formating

--- htmlparse.py
+++ htmlparse.py
@@ -1,27 +1,37 @@
 import main
 from bs4 import BeautifulSoup as bs
 import requests
-import re
 
 def getDefinition():
     # grabs the full html of the url
     source = requests.get(main.Dict.sendUrl()).text
     # parses the html
     soup = bs(source, 'lxml')
-    # grab section of html
-    string_section = soup.find_all('span', attrs={'class':'dtText'})
-    return string_section
+    # other alternatives
+    #defSection = soup.find_all('div', attrs={'id':'dictionary-entry-1'})
+    #defSection = soup.find('div', attrs={'id':'dictionary-entry-1'}).stripped_strings
+    # extracts the dictionary definitions
+    defSection = soup.find_all('span', attrs={'class':'dtText'})
+    return defSection 
 
 
 def parse():
-    text = getDefinition()
-    for snippets in text:
-        print(snippets.get_text())
-    #print(text[0].find_all('span', attrs={'class':'dtText'}).text)
-    #return 0
+    defText = getDefinition()
+    formatedDefinition = ''
+    # parses the list of dictionary definitions
+    for text in defText:
+        formatedDefinition += '\n' + text.get_text()
+    # calls function to put it all together and print to file
+    writeToDictFile(formatedDefinition)
 
 
-def writeToDictFile():
+def writeToDictFile(formatedDefinition):
+    # some formating
+    word = main.Dict.paste().upper()
+    dictHeader = '<' + 5*'-' + 'Definition of ' + word + 5*'-' + '>'
+    dictCapstone = '\n<' + (26+len(word))*'-' + '>\n\n\n'
+    # putting it all together
+    dictEntry = dictHeader + formatedDefinition + dictCapstone
     # opens up a file to append the dictionary definitions
     with open('Dictionary.txt', 'a') as f:
-        f.write(definition)
+        f.write(dictEntry)
